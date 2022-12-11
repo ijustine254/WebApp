055ecbb291b76004453b7e2b87cc012f6bf2a489
Justine <ontita254@gmail.com>
2022-12-11 19:47:33 +0300

    hide secret

--- settings.py
+++ settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d(45++c)z*%_$f5$3)63i_$tj746yew8!4$k_(s6-7-!895#-t'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
